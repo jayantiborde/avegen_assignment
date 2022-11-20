f46583737abb69fd8b01d7580624a799f7630634
unknown <jayantiborde1993@gmail.com>
2022-11-20 21:14:14 +0530

    Final Submition

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -12,14 +12,14 @@ import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class BaseClass {
+public class LoginTests {
 	DesiredCapabilities cap;
 	AppiumDriver<MobileElement> appiumDriver;
+	static String APPIUM_SERVER_URL = "http://127.0.0.1:4723/wd/hub";
 
-	@Test(priority = 1)
+	@Test(priority = 1, groups = { "skipFlow", "googleLoginFlow" })
 	public void setCapabilties() throws Exception {
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Mi A3");
@@ -29,21 +29,21 @@ public class BaseClass {
 		cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60");
 		cap.setCapability("appPackage", "com.avegen.together");
 		cap.setCapability("appActivity", "com.avegen.together.MainActivity");
-		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		URL url = new URL(LoginTests.APPIUM_SERVER_URL);
 		appiumDriver = new AppiumDriver<MobileElement>(url, cap);
 		Thread.sleep(5000);
 		// Setting implicit timeout between 2 actions and redirections in 2 pages
 		appiumDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2, groups = { "skipFlow", "googleLoginFlow" })
 	public void selLanguage() {
 		// Select Language Option
 		appiumDriver.findElement(By.xpath("//android.view.ViewGroup[@index='4']")).click();
 		appiumDriver.findElement(By.xpath("//android.view.ViewGroup[@index='5']")).click();
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 3, groups = { "skipFlow", "googleLoginFlow" })
 	public void enterName() {
 		// Enter Name
 		appiumDriver.findElement(By.xpath("//android.widget.EditText[@index='2']")).sendKeys("Jayanti More");
@@ -52,30 +52,33 @@ public class BaseClass {
 				.click();
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 4, groups = {"googleLoginFlow" })
 	public void selectMonth() {
 		// Please select your current month of pregnancy
 		appiumDriver.findElement(By.xpath("//android.widget.TextView[@text='9']")).click();
 		appiumDriver.findElement(By.xpath(
 				"//android.view.ViewGroup[@index='17']//android.view.ViewGroup[@index='0']//android.view.ViewGroup[@index='1']"))
 				.click();
-		
-	}
-	
 
-	@Test(priority = 5)
-	public void enterMobileLogin() {
-		appiumDriver.findElement(By.xpath("//android.widget.EditText[@text='Enter your number']"))
-				.sendKeys("9172164861");
-		appiumDriver.executeScript("mobile: performEditorAction", ImmutableMap.of("action", "Done"));
-//click on next button
-//		appiumDriver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
-//				+ "/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/"
-//				+ "android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]"
-//				+ "/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup"
-//				+ "/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]"))
-//				.click();
-		//click on signIn option
+	}
+	@Test(priority = 4, groups = { "skipFlowDate","skipFlow" })
+	public void selectDate() {
+		appiumDriver.findElement(By.xpath("//android.view.ViewGroup[@index='16']")).click();
+		appiumDriver.findElement(By.xpath(
+				"//android.widget.Button[@content-desc=\" Wednesday 23 November 2022 \"]/android.widget.TextView"))
+				.click();
+		appiumDriver.findElement(By.xpath("//android.widget.TextView[@text='OK']")).click();
+		appiumDriver.findElement(By.xpath(
+				"//android.view.ViewGroup[@index='17']//android.view.ViewGroup[@index='0']//android.view.ViewGroup[@index='1']"))
+				.click();
+	}
+
+	@Test(priority = 5, groups = { "googleLoginFlow" })
+	public void signInWithGoogle() {
+//		appiumDriver.findElement(By.xpath("//android.widget.EditText[@text='Enter your number']"))
+//				.sendKeys("9172164861");
+//		appiumDriver.executeScript("mobile: performEditorAction", ImmutableMap.of("action", "Done"));
+		// click on signIn option
 		appiumDriver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
 				+ "/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout"
 				+ "/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]"
@@ -86,34 +89,23 @@ public class BaseClass {
 				+ "/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout"
 				+ "/android.widget.LinearLayout/android.support.v7.widget.RecyclerView/android.widget.LinearLayout[1]"
 				+ "/android.widget.LinearLayout")).click();
-		appiumDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		appiumDriver.manage().timeouts().implicitlyWait(9, TimeUnit.SECONDS);
 	}
-	
-	
 
-	@Ignore
+	@Test(priority = 6, groups = { "skipFlow" })
 	public void testForSKip() {
 		// Enter your mobile number: Skip
 		appiumDriver.findElement(By.xpath("//android.widget.TextView[@text='Skip']")).click();
 
 	}
 
-	@Test(priority = 6)
+	@Test(priority = 7, groups = { "skipFlow", "googleLoginFlow" })
 	public void clickOnPopUp() {
-		String g1=appiumDriver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
-				+ "/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup"
-				+ "/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView[1]")).getText();
-		//String g1=	appiumDriver.findElement(By.xpath("//android.widget.TextView[@text='Allow us to access your location while you are using the app?']")).getText();
-	System.out.println(g1);	
-	// click on Don't Allow Button
+		// click on Don't Allow Button
 		appiumDriver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout"
 				+ "/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup"
 				+ "/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.HorizontalScrollView"
 				+ "/android.view.ViewGroup/android.view.ViewGroup[1]")).click();
-	
-	}
-
-	public void tearDown() {
 
 	}
 
